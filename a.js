e3394e631fd910c294d2a8e013631f2c995eba1b
smulik shotlandgit config --global user.name smulik <shmolik1990@gmail.com>
2020-06-18 15:33:45 +0300

    galery

---
+++ a.js
@@ -0,0 +1,106 @@
+let mechonit = [
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/220px-FoS20162016_0624_132444AA_%2827785299372%29.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRewHEgr8Mih73ZwTYjiHeb0aO2PzwbyhiCcL9e9OPS1hOXn1Vh&usqp=CAU',
+    'https://images1.calcalist.co.il/PicServer2/20122005/213986/2_l.jpg',
+    'https://lh3.googleusercontent.com/proxy/kCe9PkCWdguwTtCrjQFxpBQ71iqGWFadfcX2QpwLJk_tv5VYczhjQ8g1hRgQE_iaFW_JOMUXlppI-xvBnwH7g06BcWIhn3As6cVlk_UKMC14UICS25jkTTbS',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/220px-FoS20162016_0624_132444AA_%2827785299372%29.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRewHEgr8Mih73ZwTYjiHeb0aO2PzwbyhiCcL9e9OPS1hOXn1Vh&usqp=CAU',
+    'https://images1.calcalist.co.il/PicServer2/20122005/213986/2_l.jpg',
+    'https://lh3.googleusercontent.com/proxy/kCe9PkCWdguwTtCrjQFxpBQ71iqGWFadfcX2QpwLJk_tv5VYczhjQ8g1hRgQE_iaFW_JOMUXlppI-xvBnwH7g06BcWIhn3As6cVlk_UKMC14UICS25jkTTbS',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/220px-FoS20162016_0624_132444AA_%2827785299372%29.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRewHEgr8Mih73ZwTYjiHeb0aO2PzwbyhiCcL9e9OPS1hOXn1Vh&usqp=CAU',
+    'https://images1.calcalist.co.il/PicServer2/20122005/213986/2_l.jpg',
+    'https://lh3.googleusercontent.com/proxy/kCe9PkCWdguwTtCrjQFxpBQ71iqGWFadfcX2QpwLJk_tv5VYczhjQ8g1hRgQE_iaFW_JOMUXlppI-xvBnwH7g06BcWIhn3As6cVlk_UKMC14UICS25jkTTbS',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/220px-FoS20162016_0624_132444AA_%2827785299372%29.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRewHEgr8Mih73ZwTYjiHeb0aO2PzwbyhiCcL9e9OPS1hOXn1Vh&usqp=CAU',
+    'https://images1.calcalist.co.il/PicServer2/20122005/213986/2_l.jpg'
+    
+
+
+
+]
+let chayot =['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3pazdoeMu5WrrbWltJIzOTRk14_UVTF7o2Y1He5_ypu9DIxiD&usqp=CAU',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/260px-Grosser_Panda.JPG',
+'https://lh3.googleusercontent.com/proxy/lmii_A5Ti0OAxZ6khrZ1_Msteg2k1Sx2W4OnnLBdwt6KM7AOPRzD2YgsNUDNbCsGYERPa5RwyYbrLH-znga6bEaIXygqnH2j41006ai9cgWGW0rla6WHlsMLA3WE_IPdkosUrr9h9_F4M92sU-I',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3pazdoeMu5WrrbWltJIzOTRk14_UVTF7o2Y1He5_ypu9DIxiD&usqp=CAU',
+'https://www.shishibagolan.co.il/wp-content/uploads/2017/08/%D7%97%D7%99%D7%95%D7%AA-%D7%91%D7%A8-1.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/260px-Grosser_Panda.JPG',
+'https://lh3.googleusercontent.com/proxy/lmii_A5Ti0OAxZ6khrZ1_Msteg2k1Sx2W4OnnLBdwt6KM7AOPRzD2YgsNUDNbCsGYERPa5RwyYbrLH-znga6bEaIXygqnH2j41006ai9cgWGW0rla6WHlsMLA3WE_IPdkosUrr9h9_F4M92sU-I',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3pazdoeMu5WrrbWltJIzOTRk14_UVTF7o2Y1He5_ypu9DIxiD&usqp=CAU',
+'https://www.shishibagolan.co.il/wp-content/uploads/2017/08/%D7%97%D7%99%D7%95%D7%AA-%D7%91%D7%A8-1.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/260px-Grosser_Panda.JPG',
+'https://lh3.googleusercontent.com/proxy/lmii_A5Ti0OAxZ6khrZ1_Msteg2k1Sx2W4OnnLBdwt6KM7AOPRzD2YgsNUDNbCsGYERPa5RwyYbrLH-znga6bEaIXygqnH2j41006ai9cgWGW0rla6WHlsMLA3WE_IPdkosUrr9h9_F4M92sU-I',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3pazdoeMu5WrrbWltJIzOTRk14_UVTF7o2Y1He5_ypu9DIxiD&usqp=CAU',
+'https://www.shishibagolan.co.il/wp-content/uploads/2017/08/%D7%97%D7%99%D7%95%D7%AA-%D7%91%D7%A8-1.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/260px-Grosser_Panda.JPG'
+
+
+
+]
+let dagim =['https://www.ac-fish.co.il/cwsd.php?Z3AuPTQ0Pw/NDY/YmxgZyk2MzE5NzIkUDU4MT15fWQ.jpg',
+'https://www.discus.co.il/he_IL/%D7%93%D7%92%D7%99_%D7%A0%D7%95%D7%99/images/%D7%93%D7%92%D7%99-%D7%96%D7%94%D7%91/%D7%93%D7%92-%D7%96%D7%94%D7%91-%D7%A7%D7%95%D7%9E%D7%98.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Sixfinger_threadfin_school.jpg/270px-Sixfinger_threadfin_school.jpg',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQCgrahxX2m7MG-byz-YiPJGToTXD_-xbhlZAxrRHGUCNWd0Y1k&usqp=CAU',
+'https://www.ac-fish.co.il/cwsd.php?Z3AuPTQ0Pw/NDY/YmxgZyk2MzE5NzIkUDU4MT15fWQ.jpg',
+'https://www.discus.co.il/he_IL/%D7%93%D7%92%D7%99_%D7%A0%D7%95%D7%99/images/%D7%93%D7%92%D7%99-%D7%96%D7%94%D7%91/%D7%93%D7%92-%D7%96%D7%94%D7%91-%D7%A7%D7%95%D7%9E%D7%98.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Sixfinger_threadfin_school.jpg/270px-Sixfinger_threadfin_school.jpg',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQCgrahxX2m7MG-byz-YiPJGToTXD_-xbhlZAxrRHGUCNWd0Y1k&usqp=CAU',
+'https://www.ac-fish.co.il/cwsd.php?Z3AuPTQ0Pw/NDY/YmxgZyk2MzE5NzIkUDU4MT15fWQ.jpg',
+'https://www.discus.co.il/he_IL/%D7%93%D7%92%D7%99_%D7%A0%D7%95%D7%99/images/%D7%93%D7%92%D7%99-%D7%96%D7%94%D7%91/%D7%93%D7%92-%D7%96%D7%94%D7%91-%D7%A7%D7%95%D7%9E%D7%98.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Sixfinger_threadfin_school.jpg/270px-Sixfinger_threadfin_school.jpg',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQCgrahxX2m7MG-byz-YiPJGToTXD_-xbhlZAxrRHGUCNWd0Y1k&usqp=CAU',
+'https://www.ac-fish.co.il/cwsd.php?Z3AuPTQ0Pw/NDY/YmxgZyk2MzE5NzIkUDU4MT15fWQ.jpg',
+'https://www.discus.co.il/he_IL/%D7%93%D7%92%D7%99_%D7%A0%D7%95%D7%99/images/%D7%93%D7%92%D7%99-%D7%96%D7%94%D7%91/%D7%93%D7%92-%D7%96%D7%94%D7%91-%D7%A7%D7%95%D7%9E%D7%98.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Sixfinger_threadfin_school.jpg/270px-Sixfinger_threadfin_school.jpg'
+
+
+
+
+]
+
+
+let gallery1 = document.querySelector('#gallery1')
+let gallery2 = document.querySelector('#gallery2')
+let gallery3 = document.querySelector('#gallery3')
+
+
+mechonit.forEach( (p) =>
+  gallery1.innerHTML += `<img src="${p}"/>`
+
+)
+chayot.forEach( (p) =>
+  gallery2.innerHTML += `<img src="${p}"/>`
+
+)
+dagim.forEach( (p) =>
+  gallery3.innerHTML += `<img src="${p}"/>`
+
+)
+let liIndex = 0
+let limax = document.querySelectorAll('.app img').length-1
+
+function showItem(){
+    document.querySelectorAll('img')
+        .forEach(e=> 
+            e.className = e.className.replace('hide', ''))
+
+  let needTo_hideli = document.querySelectorAll('.app img')[liIndex]
+  needTo_hideli.className = needTo_hideli.className.replace('show', 'hide')
+
+  if (liIndex >= limax) {
+      liIndex = -1
+  }
+
+  let needTo_showli = document.querySelectorAll('img')[++liIndex]
+  needTo_showli.className = 'show'
+  
+}
+//showItem()
+setInterval(showItem, 1600)
+
+
+
+
+
+
+
